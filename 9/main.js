3b1b45218806b9532cc72d43651902aa712fdccb
Ligeon <ligeon2@gmail.com>
2024-02-08 19:14:02 +0200

    9 done

---
+++ 9/main.js
@@ -0,0 +1,14 @@
+let num = prompt('num')
+let deg = prompt('deg')
+function func(num, deg) {
+    if (deg === 1) return num
+    else {
+        return num * func(num, deg-1)
+    }
+}
+alert(func(num,deg))
+
+let reloadBtn = document.getElementById('reload')
+reloadBtn.addEventListener('click', () => {
+    location.reload()
+})
