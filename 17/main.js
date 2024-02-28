ab970c8b0d43052ff810443bf831d290d7aee9b4
Ligeon <ligeon2@gmail.com>
2024-02-28 18:31:58 +0200

    17 fixed

--- 17/main.js
+++ 17/main.js
@@ -1,11 +1,18 @@
-let array = [[5, 12, 'string', true,[10]], 1, 2, 3, '2', 4, false, 10.5]
+let array = [[5, 12, 'string', true, [10]], 1, 2, 3, '2', 4, false, 10.5]
+let numbersAmount = 0
 let sum = 0
+
 function recursion(arr) {
     return arr.map((item) => {
-        if (typeof item === 'number') return sum+=item
+        if (typeof item === 'number') {
+            numbersAmount++
+            return sum += item
+        }
         if (typeof item === 'object') return recursion(item)
     })
 }
 
 console.log(recursion(array))
-console.log(sum)
+console.log('sum = ' + sum)
+console.log('amount of numbers = ' + numbersAmount)
+console.log('result = ' + sum / numbersAmount)
